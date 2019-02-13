df14cd10716c8566550a06b3a8c42e53783ecd1d
Joseph Surin <joseph.surin@gmail.com>
2019-02-13 19:24:31 +1100

    clean up core.js

--- src/core.js
+++ src/core.js
@@ -2,8 +2,7 @@ const fs = require('graceful-fs')
 const request = require('request')
 import { bindNodeCallback, from } from 'rxjs'
 import { map, pluck, tap, mergeMap, filter, scan, finalize, mergeAll, } from 'rxjs/operators'
-import { sudPath, getRangeHeaders, createRequest } from './util'
-import { partialPath, getLocalFilesize } from '../dist/util'
+import { sudPath, calculateRanges, getRangeHeaders, createRequest, partialPath, getLocalFilesize  } from './util'
 
 //the observable created from the requestHead function will emit the array [response, ''] if no error is caught
 //because the parameters for the request callback are (err, response, body) and body is empty
@@ -20,17 +19,9 @@ export function getRemoteFilesize(url) {
 export function getMetadata(url, savePath, threads, filesize$) {
 	return filesize$.pipe(
 		map(filesize => {
-			var ranges = new Array(threads)
-			if(threads == 1) {
-				ranges = [[0, filesize]]
-			} else {
-				var partitionSize = Math.floor(filesize / threads)
-				ranges[0] = [0, partitionSize]
-				for(var i = 1; i < threads - 1; i++) {
-					ranges[i] = [(partitionSize * i) + 1, partitionSize * (i + 1)]
-				}
-				ranges[threads - 1] = [ranges[threads - 2][1] + 1, filesize]
-			}
+
+			var ranges = calculateRanges(filesize, threads)
+
 			var meta = {
 				url,
 				savePath,
@@ -38,8 +29,10 @@ export function getMetadata(url, savePath, threads, filesize$) {
 				filesize,
 				ranges
 			}
+
 			return meta
 		}),
+
 		//write data to .sud meta file side effect
 		tap(meta => {
 			fs.writeFile(meta.sudPath, JSON.stringify(meta))
@@ -47,32 +40,39 @@ export function getMetadata(url, savePath, threads, filesize$) {
 	)
 }
 
+//calculate ranges based on existing .PARTIAL files
+//and transform the meta object into an object holding an array of request observables
+//and the meta data object
 export function makeRequests(meta$, optionalHeaders) {
 	return meta$.pipe(
-		//calculate ranges based on existing .PARTIAL files
-		//and transform the meta object into an object holding an array of request observables
-		//and the meta data object
 		map(meta => {
+
 			var { url, savePath, ranges } = meta
 			var rangeHeaders = getRangeHeaders(savePath, ranges)
 			var request$s = new Array(rangeHeaders.length)
+
 			rangeHeaders.forEach((rangeHeader, index) => {
 				request$s[index] = createRequest(url, Object.assign(optionalHeaders || {}, { range: rangeHeader }))
 			})
+
 			return { request$s, meta }
 		})
 	)
 }
 
+//write to buffer side effect within and merge to higher-order positions observable
 export function getThreadPositions(requestsAndMeta$) {
-	//write to buffer side effect within merge to higher-order positions observable
 	return requestsAndMeta$.pipe(
 		mergeMap(requestsAndMeta => {
-			var { request$s, meta: { savePath, ranges } } = requestsAndMeta 
+
+			var { request$s, meta: { savePath, ranges } } = requestsAndMeta
+
 			var transformedRequest$s = request$s.map((request$, index) => {
+
 				var partialFile = partialPath(savePath, index)
 				var startPos = ranges[index][0] + getLocalFilesize(partialFile)
 				var writeStream = fs.createWriteStream(partialFile, { flags: 'a', start: startPos })
+
 				return request$.pipe(
 					filter(x => x.event == 'data'),
 					pluck('data'),
@@ -81,8 +81,10 @@ export function getThreadPositions(requestsAndMeta$) {
 					finalize(() => writeStream.end())
 				)
 			})
+
 			return from(transformedRequest$s)
 		}),
+
 		//merge all position observables into one observable
 		mergeAll()
 	)
--- src/util.js
+++ src/util.js
@@ -18,6 +18,25 @@ export function createRequest(url, headers) {
 
 export const getLocalFilesize = filename => fs.existsSync(filename) ? fs.statSync(filename).size : 0
 
+//takes a file size and the number of partial files (threads) to be used
+//and calculates the ranges in bytes for each thread
+export function calculateRanges(filesize, threads) {
+	var ranges = new Array(threads)
+
+	if(threads == 1) {
+		ranges = [[0, filesize]]
+	} else {
+		var partitionSize = Math.floor(filesize / threads)
+		ranges[0] = [0, partitionSize]
+		for(var i = 1; i < threads - 1; i++) {
+			ranges[i] = [(partitionSize * i) + 1, partitionSize * (i + 1)]
+		}
+		ranges[threads - 1] = [ranges[threads - 2][1] + 1, filesize]
+	}
+
+	return ranges
+}
+
 //position starts counting at 0, i.e. the size of the .PARTIAL file
 export function toRangeHeader(range, position) {
 	var start = range[0] + position
