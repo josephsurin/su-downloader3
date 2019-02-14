5306ace52e17226ff82a1d56aef93b5e3648df38
Joseph Surin <joseph.surin@gmail.com>
2019-02-14 17:38:43 +1100

    destructuring for default options parameter

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import { getRemoteFilesize, getMetadata, makeRequests, getThreadPositions, getDownloadProgressInfo } from './core'
 
-const defaultOptions = {
-	threads: 4,
-	timeout: 3 * 60 * 1000,
-	headers: null,
-	throttleRate: 500
-}
-
-function startDownload(url, savePath, options = defaultOptions) {
+function startDownload(url, savePath, { threads = 4, timeout = 3*60*1000, headers = null, throttleRate = 500 } = {}) {
 
 	var filesize$ = getRemoteFilesize(url)
 
-	var meta$ = getMetadata(url, savePath, options.threads, filesize$)
+	var meta$ = getMetadata(url, savePath, threads, filesize$)
 
-	var requestsAndMeta$ = makeRequests(meta$, options)
+	var requestsAndMeta$ = makeRequests(meta$, { timeout, headers })
 
 	var threadPositions$ = getThreadPositions(requestsAndMeta$)
 
-	var downloadProgressInfo$ = getDownloadProgressInfo(threadPositions$, options.throttleRate)
+	var downloadProgressInfo$ = getDownloadProgressInfo(threadPositions$, throttleRate)
 
 	//the first value emitted is the meta data object
 	return downloadProgressInfo$
