fb946322b05b6d41c97216a1ba68704a31cf3e70
Joseph Surin <joseph.surin@gmail.com>
2019-02-16 06:15:30 +1100

    update cli arguments to reflect save location changes

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 
 var program = require('commander')
 
+const path = require('path')
 const fs = require('graceful-fs')
 const { startDownload } = require('../build/downloader')
 const observer = require('./observer')()
@@ -10,9 +11,11 @@ program
 	.version('1.0.0')
 
 program
-	.command('start [url]')
-	.description('start a new download')
-	.option('-s', '--save-path [save_path]', 'relative path to save file to')
+	.command('start <url>')
+	.description('start a new download, only one of -S, -s and -d should be specified')
+	.option('-S, --saveas [save_name]', 'filename to save the file as in the current working directory')
+	.option('-s, --savepath [save_path]', 'full path to save file to')
+	.option('-d, --savedir [save_dir]', 'directory to save file to (generated filename)')
 	.option('-t, --threads [threads]', 'number of threads to use')
 	.option('-T, --timeout [timeout]', 'how long to maintain connection before aborting (ms)')
 	.option('-H, --headers [headers]', 'HTTP request headers')
@@ -22,19 +25,23 @@ program
 			console.error('a url is required, type sud3 start --help for help')
 			process.exit(1)
 		}
+		var saveAs = null
 		var savePath = null
+		var saveDir = null
 		if(options) {
-			if(options.save_path) savePath = options.save_path
+			if(options.saveas) savePath = path.join(process.cwd(), options.saveas)
+			if(options.savepath) savePath = options.savepath
+			if(options.savedir) saveDir = options.savedir
 			if(options.threads) options.threads = parseInt(options.threads)
 			if(options.timeout) options.timeout = parseInt(options.timeout)
 			if(options.headers) options.headers = JSON.parse(options.headers)
 			if(options.throttleRate) options.throttleRate = parseInt(options.throttleRate)
 		}
-		startDownload({ url, savePath }, options).subscribe(observer)
+		startDownload({ url, savePath, saveDir }, options).subscribe(observer)
 	})
 
 program
-	.command('resume [sud_path]')
+	.command('resume <sud_path>')
 	.description('resume a download from an existing .sud file')
 	.option('-T, --timeout [timeout]', 'how long to maintain connection before aborting (ms)')
 	.option('-H, --headers [headers]', 'HTTP request headers')
