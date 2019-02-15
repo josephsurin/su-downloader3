c40c8adfc6f7b15882aee21f90e0a04a27573c9c
Joseph Surin <joseph.surin@gmail.com>
2019-02-16 06:43:23 +1100

    ci, readme, entry point, exports

---
+++ README.md
@@ -0,0 +1,4 @@
+# su-downloader3 node.js HTTP downloader
+
+![https://nodei.co/npm/su-downloader3](https://nodei.co/npm/su-downloader3.png?downloads=true&downloadRank=true&stars=true)
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { startDownload, killFiles, sudPath } from './downloader'
+import SuDScheduler from './scheduler'
+
+module.exports = {
+	startDownload,
+	killFiles,
+	sudPath,
+	SuDScheduler
+}
