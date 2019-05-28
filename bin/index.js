92ebaeb10ae955ab091c22f49e35206a6eecfb0e
fumetsuu <joseph.surin@gmail.com>
2019-05-28 16:46:35 +1000

    update version

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,7 @@ const observer = require('./observer')(true)
 const observerSched = require('./observer')(false)
 
 program
-	.version('1.1.0')
+	.version('1.1.1')
 
 program
 	.command('start <url>')
